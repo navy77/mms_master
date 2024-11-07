3c3e99e837f454fd96124ac1fe83f9f26b09b8b5
arthit.s <arthit.sekmoh@gmail.com>
2024-11-07 14:34:43 +0700

    Update autodrop.py

--- autodrop.py
+++ autodrop.py
@@ -4,7 +4,7 @@ import os
 import time
 def drop_mqtt_consumer():
     # client = InfluxDBClient(host='192.168.0.160', port=8086, username='admin', password='admin', database='influx')
-    time.sleep(30)
+    time.sleep(120)
     client = InfluxDBClient(os.environ["INFLUX_SERVER"], os.environ["INFLUX_PORT"], os.environ["INFLUX_USER_LOGIN"], os.environ["INFLUX_PASSWORD"], os.environ["INFLUX_DATABASE"])
     client.query('DROP MEASUREMENT mqtt_consumer')
     print("Dropped measurement mqtt_consumer")
