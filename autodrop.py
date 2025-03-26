e3b291e1cbd4850e4c57292a15b544fa1ecb3da6
navy77 <arthit.sekmoh@gmail.com>
2025-03-26 11:17:54 +0700

    add prevent autodrop working

--- autodrop.py
+++ autodrop.py
@@ -2,13 +2,19 @@ from influxdb import InfluxDBClient
 import dotenv
 import os
 import time
+import datetime
+
 def drop_mqtt_consumer():
-    # client = InfluxDBClient(host='192.168.0.160', port=8086, username='admin', password='admin', database='influx')
-    time.sleep(120)
-    client = InfluxDBClient(os.environ["INFLUX_SERVER"], os.environ["INFLUX_PORT"], os.environ["INFLUX_USER_LOGIN"], os.environ["INFLUX_PASSWORD"], os.environ["INFLUX_DATABASE"])
-    client.query('DROP MEASUREMENT mqtt_consumer')
-    print("Dropped measurement mqtt_consumer")
-    client.close()
+
+    day = datetime.datetime.now().strftime("%d")
+    if day == '26':
+        # client = InfluxDBClient(host='192.168.0.180', port=8087, username='admin', password='admin', database='influx')
+        time.sleep(1)
+        client = InfluxDBClient(os.environ["INFLUX_SERVER"], os.environ["INFLUX_PORT"], os.environ["INFLUX_USER_LOGIN"], os.environ["INFLUX_PASSWORD"], os.environ["INFLUX_DATABASE"])
+        client.query('DROP MEASUREMENT mqtt_consumer')
+        print("Dropped measurement mqtt_consumer")
+        client.close()
+    else:print("not 1st day")
 
 if __name__ == "__main__":
     dotenv_file = dotenv.find_dotenv()
